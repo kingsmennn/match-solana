f99012f5326eb506d472821295c1f60bf4ae34ba
Akachukwu Chukwuemerie David <63329343+Imdavyking@users.noreply.github.com>
2024-09-04 16:10:17 +0100

    adding solana providers

---
+++ pages/wallet_provider.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+  useAnchorWallet,
+  useConnection,
+  useWallet,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+
+import {
+  // GlowWalletAdapter,
+  PhantomWalletAdapter,
+  CloverWalletAdapter,
+  LedgerWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+  TrustWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+import { clusterApiUrl } from "@solana/web3.js";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import * as anchor from "@project-serum/anchor";
+import { marketAbi } from "../blockchain/abi";
+import {
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+import { utf8 } from "@project-serum/anchor/dist/cjs/utils/bytes";
+import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey";
+
+import { BN } from "@project-serum/anchor";
+
+interface WalletConnectProviderProps {
+  children: any;
+}
+</script>
