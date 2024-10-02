2a24c702a09bdea6d184d3074d5a9edd5750578d
imdavyking <yungdynamic53@gmail.com>
2024-10-02 08:25:39 +0100

    removed unused console.log

--- pinia/request.ts
+++ pinia/request.ts
@@ -570,7 +570,6 @@ export const useRequestsStore = defineStore("requests", {
           throw new Error("use payForRequestToken instead");
         }
 
-        console.log(PORTAL_PYUSD_TOKEN_ACCOUNT.toBase58());
         const contract = await userStore.getContract();
         const requestMade = await contract.account.request.all([
           {
